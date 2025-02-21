042b27c6ee4440461a3eab317d077218f5b3670e
Романов Кирилл Александрович <k.romanov@corp.sg.local>
2025-02-21 14:18:37 +0300

    Eslint упростил проверку

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Отключаем проверку ESLint при production сборке
+    ignoreDuringBuilds: true,
+  },
+  // Отключаем строгий режим для упрощения разработки
+  reactStrictMode: false,
+  // Разрешаем использование внешних изображений
   images: {
     domains: ['images.unsplash.com'], // Разрешаем загрузку изображений с unsplash
   },
